5a8adf17a846c3eb62df7868d8907eb66115d14a
Kushal Neupane <mkushal10@gmail.com>
2020-08-09 22:37:34 +0545

    login workred

--- Restaurant Management System/Controllers/LoginController.cs
+++ Restaurant Management System/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurant_Management_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,25 @@ namespace Restaurant_Management_System.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(Restaurant_Management_System.Models.User userModel)
+        {
+            using (RestaurantManagementSystemEntities db = new RestaurantManagementSystemEntities() )
+            {
+                var userDetails = db.Users.Where(x => x.username == userModel.username && x.password == userModel.password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["id"] = userDetails.id;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
         }
     }
 }
